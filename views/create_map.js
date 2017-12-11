0524584265d4724c2af3535305e1baf40731e538
Dylan Hong <dylan.hong@tufts.edu>
2017-12-11 16:20:49 -0500

    Placing events and showing events

--- views/create_map.js
+++ views/create_map.js
@@ -17,51 +17,7 @@ function init()
 {
 	map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
 	getMyLocation();
-	//loadEvents();
-
 }
-
-
-function getMyLocation() {
-	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
-		navigator.geolocation.getCurrentPosition(function(position) {
-			myLat = position.coords.latitude;
-			myLng = position.coords.longitude;
-			renderMap();
-		});
-	}
-	else {
-		alert("Geolocation is not supported by your web browser.  What a shame!");
-	}
-}
-
-
-function loadEvents() 
-{
-	var http = new XMLHttpRequest();
-	var response_string;
-	var url = "http://http://frozen-depths-55905.herokuapp.com/print-events";
-	http.open("GET", url, true);
-    //http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    http.onreadystatechange = function() {
-        if(http.readyState == 4 && http.status == 200) {
-            response_string = http.responseText;
-            getMyLocation(response_string);
-            sendEvents(response_string);
-        }
-    }
-    http.send(null);
-} 
-
-function sendEvents () 
-{
-	var html = "";
-
-
-	document.getElementById("list_of_event").innerHTML = "<p>Hey i made it here</p>";
-}
-
 
 function getMyLocation() {
 	if (navigator.geolocation) { // the navigator.geolocation object is supported on your browser
@@ -103,7 +59,7 @@ function renderMap()
 function placeEvents()
 {
 	var sendIt = new XMLHttpRequest();
-	var url = "https://frozen-depths-55905.herokuapp.com/";
+	var url = "https://https://frozen-depths-55905.herokuapp.com/";
 	sendIt.open("POST", url, true);
 
 	sendIt.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
@@ -148,28 +104,51 @@ function placeEvents()
 	}
 }
 
-
-//proof of concept geolocations
-// TODO: hard code all academic and res buildings on campus
-//       or at least enough to get the point accross
 var building_locations =
-{	
-  "TischLibrary" : {"lat":42.406204, "lng":-71.118877},
-  "Halligan" : {"lat":42.408215, "lng":-71.116240},
-  "574" : {"lat":42.403515, "lng":-71.113987},
-  "CampusCenter" : {"lat":42.4075, "lng":-71.1190},
-  "Carmichael Hall" : {"lat":42.409489, "lng":-71.122417},
-  "Prez Lawn" : {"lat":42.407049, "lng":-71.120636},
-  "Dewick" : {"lat":42.405327, "lng":-71.121081},
-  "TischGym" : {"lat":42.409026, "lng":-71.115491},
-  "SEC" : {"lat":42.405984, "lng":-71.116886},
-  "SoGo" : {"lat":42.404970, "lng":-71.118651},
+{
+	"TischLibrary" : {"lat":42.406204, "lng":-71.118877},
+	"Halligan" : {"lat":42.408215, "lng":-71.116240},
+	"574" : {"lat":42.403515, "lng":-71.113987},
+	"CampusCenter" : {"lat":42.4075, "lng":-71.1190},
+	"Carmichael Hall" : {"lat":42.409489, "lng":-71.122417},
+	"Prez Lawn" : {"lat":42.407049, "lng":-71.120636},
+	"Dewick" : {"lat":42.405327, "lng":-71.121081},
+	"TischGym" : {"lat":42.409026, "lng":-71.115491},
+	"SEC" : {"lat":42.405984, "lng":-71.116886},
+	"SoGo" : {"lat":42.404970, "lng":-71.118651},
 
 };
 
 
 function geolocation_of_building (building) 
 {
-	building_locations[building];
+	latLng = building_locations[building];
 	return latLng;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
