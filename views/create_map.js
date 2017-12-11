07f61a3379378ed44b7b7f90f26ce21390b25ae4
Dylan Hong <dylan.hong@tufts.edu>
2017-12-11 16:03:23 -0500

    Placing events and showing events

--- views/create_map.js
+++ views/create_map.js
@@ -103,8 +103,8 @@ function renderMap()
 function placeEvents()
 {
 	var sendIt = new XMLHttpRequest();
-	var url = "https://frozen-depths-55905.herokuapp.com/";
-	sendIt.open("POST", url, true);
+	var url = "https://frozen-depths-55905.herokuapp.com/print-events";
+	sendIt.open("GET", url, true);
 
 	sendIt.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
 
