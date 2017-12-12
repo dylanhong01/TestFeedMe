a47f3c5056c45f7eb6dd5bd5537b05ecfff89f44
Dylan Hong <dylan.hong@tufts.edu>
2017-12-11 19:39:27 -0500

    weird time test

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ app.get('/prints', function(request, response){
 
   var now = new Date();
   var eventList = [];
-  currTime = now.getHours()-5 + ':' + now.getMinutes();
+  currTime = now.getHours()-7 + ':' + now.getMinutes();
 
   console.log(currTime);
 
