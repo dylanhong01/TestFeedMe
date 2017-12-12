a4d0efdec5dded2e97a1c74f66fce5f6cf820e3a
Dylan Hong <dylan.hong@tufts.edu>
2017-12-12 14:08:14 -0500

    removed event filtering for testing

--- index.js
+++ index.js
@@ -215,9 +215,9 @@ app.post('/print', function(request, response){
       if (!err) {
         for (var count = 0; count < results.length; count++){
 
-          if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
+          //if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
             eventList.push(results[count]);
-          }
+          //}
         }
         response.send(eventList);
       }
@@ -238,9 +238,9 @@ app.get('/prints', function(request, response){
       if (!err) {
         for (var count = 0; count < results.length; count++){
 
-          if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
+          //if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
             eventList.push(results[count]);
-          }
+          //}
         }
         response.send(eventList);
       }
