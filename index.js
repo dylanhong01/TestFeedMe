35d5203301650d6aea164cd7885b48003e173667
Dylan Hong <dylan.hong@tufts.edu>
2017-12-11 22:00:45 -0500

    final bug fixes

--- index.js
+++ index.js
@@ -205,9 +205,9 @@ app.post('/print', function(request, response){
       if (!err) {
         for (var count = 0; count < results.length; count++){
 
-          if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
+          //if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
             eventList.push(results[count]);
-          }
+          //}
         }
         response.send(eventList);
       }
@@ -228,9 +228,9 @@ app.get('/prints', function(request, response){
       if (!err) {
         for (var count = 0; count < results.length; count++){
 
-          if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
+          //if (results[count].timeStart <= currTime && results[count].timeEnd >= currTime){
             eventList.push(results[count]);
-          }
+          //}
         }
         response.send(eventList);
       }
